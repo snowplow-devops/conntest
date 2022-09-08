6685724a64d2cb8ee486599f0705817743bcd39b
Piotr Limanowski <peel@users.noreply.github.com>
2022-09-08 16:53:29 +0200

    Add PR labeler and release drafter config (#1)
    * Add PR labeler and release drafter config
    
    Previously we set release information manually. Now PRs are categorized
    and added in their respective sections depending on branch name
    they originated from.
    
    For more details on how we categorize, see PR-labeler configuration:
    [[./.github/workflows/config/pr-labeler.yaml]]
    
    * Add spec for tag parsing for check cmd
    
    Previously the spec wasn't available for tag parsing. Because an
    interface and a custom Var is used for tag parsing in cobra, it
    makes sense to add the spec.
    
    * Package binary release as gzip
    
    Previously we were only uploading a binary, but it turns out that
    for some internal automation it makes sense to also upload a gzip file.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {}}:
 let
 
-  inherit (pkgs) go golangci-lint gopls gore gotools go-tools usql cobra-cli;
+  inherit (pkgs) go golangci-lint gopls gore gotools go-tools usql cobra-cli coreutils;
 in pkgs.mkShell {
-  buildInputs = [ go golangci-lint gopls gore gotools go-tools usql cobra-cli];
+  buildInputs = [ go golangci-lint gopls gore gotools go-tools usql cobra-cli coreutils];
 }
