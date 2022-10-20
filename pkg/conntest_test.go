1b43db5abc5b3b551b2787eb59529c0a39ab245c
Piotr Limanowski <peel@users.noreply.github.com>
2022-10-20 09:03:34 +0200

    Add copyright headers (#6)
    For consistency we need to keep those around for all the code in
    this repo.

--- pkg/conntest_test.go
+++ pkg/conntest_test.go
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2022 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
 package pkg
 
 import (
@@ -29,19 +42,3 @@ func TestMarshall(t *testing.T) {
 		t.Fail()
 	}
 }
-
-// func TestParseTags(t *testing.T) {
-// 	expected := map[string]string{"dolor": "sit-amet", "lorem": "ipsum"}
-// 	sample := "lorem=ipsum;dolor=sit-amet"
-// 	actuals := []map[string]string{
-// 		ParseTags(sample),
-// 		ParseTags(";" + sample + ";"),
-// 	}
-
-// 	for i, actual := range actuals {
-// 		if !reflect.DeepEqual(actual, expected) {
-// 			t.Log("error at", i, ":"8, actual, expected)
-// 			t.Fail()
-// 		}
-// 	}
-// }
